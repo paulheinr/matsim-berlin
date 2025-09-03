a5740f55015e0bf70b386dac7a5d1e82d89a1c16
rakow <rakow@vsp.tu-berlin.de>
2025-09-03 09:36:06 +0200

    install paralell request inserter module

--- src/main/java/org/matsim/legacy/run/drt/RunDrtOpenBerlinScenario.java
+++ src/main/java/org/matsim/legacy/run/drt/RunDrtOpenBerlinScenario.java
@@ -28,6 +28,7 @@ import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.drt.optimizer.insertion.parallel.ParallelRequestInserterModule;
 import org.matsim.contrib.drt.run.DrtConfigGroup;
 import org.matsim.contrib.drt.run.DrtConfigs;
 import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
@@ -112,6 +113,15 @@ public final class RunDrtOpenBerlinScenario {
 		// yyyy there is fareSModule (with S) in config. ?!?!  kai, jul'19
 		controler.addOverridingModule(new IntermodalTripFareCompensatorsModule());
 		controler.addOverridingModule(new PtIntermodalRoutingModesModule());
+
+
+		for (DrtConfigGroup drtConfig : MultiModeDrtConfigGroup.get(controler.getConfig()).getModalElements()) {
+			if (drtConfig.getDrtParallelInserterParams().isPresent()) {
+				log.info("Activating parallel drt request inserter for {}", drtConfig.getMode());
+				controler.addOverridingQSimModule(new ParallelRequestInserterModule(drtConfig));
+			}
+		}
+
 
 		return controler;
 	}
