b6ca1afd8dc19b51514924792c05ea3550f5c783
sime94 <meinhardt@vsp.tu-berlin.de>
2025-10-21 16:53:50 +0200

    add empty scenario class for drt scenario

---
+++ src/main/java/org/matsim/run/gartenfeld/GartenfeldDrtScenario.java
@@ -0,0 +1,14 @@
+package org.matsim.run.gartenfeld;
+
+
+/**
+ * Scenario class for Gartenfeld DRT scenario.
+ * <p>
+ * Like its parent class GartenfeldScenario, this scenario has its own input files, which extend the OpenBerlin scenario files with inhabitants and road infrastructure specific to Gartenfeld.
+ * See {@link org.matsim.prepare.gartenfeld.CreateGartenfeldComplete} for the creation of these input files.
+ */
+public class GartenfeldDrtScenario extends GartenfeldScenario {
+
+//	TODO
+
+}
