8aa89cbfbee846458fe850d434edb3640ac54f2f
sime94 <meinhardt@vsp.tu-berlin.de>
2025-12-03 14:43:24 +0100

    introduce GartenfeldUtils class

---
+++ src/main/java/org/matsim/utils/GartenfeldUtils.java
@@ -0,0 +1,12 @@
+package org.matsim.utils;
+
+public class GartenfeldUtils {
+	private GartenfeldUtils() {
+
+	}
+
+	/**
+	 * Helper enum to enable/disable functionalities.
+	 */
+	public enum FunctionalityHandling {ENABLED, DISABLED}
+}
