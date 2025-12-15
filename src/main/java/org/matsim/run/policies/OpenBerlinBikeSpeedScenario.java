4ea37513a17d542cf6767ebaceb581c12eaa2910
sime94 <meinhardt@vsp.tu-berlin.de>
2025-12-15 16:42:23 +0100

    finalize bike speed scenario run class

--- src/main/java/org/matsim/run/policies/OpenBerlinBikeSpeedScenario.java
+++ src/main/java/org/matsim/run/policies/OpenBerlinBikeSpeedScenario.java
@@ -2,10 +2,13 @@ package org.matsim.run.policies;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.Controler;
 import org.matsim.run.OpenBerlinScenario;
+import org.matsim.vehicles.VehicleType;
 import picocli.CommandLine;
 
 import javax.annotation.Nullable;
@@ -17,13 +20,22 @@ import javax.annotation.Nullable;
 public class OpenBerlinBikeSpeedScenario extends OpenBerlinScenario {
 	Logger log = LogManager.getLogger(OpenBerlinBikeSpeedScenario.class);
 
+	@CommandLine.Option(names = "--max-bike-speed", description = "Defines to which value in km/h the maximum velocity of bikes is set. Default = 10.728 km/h", defaultValue = "10.728")
+	private double maxBikeSpeedKmH;
+
 	@Nullable
 	@Override
 	public Config prepareConfig(Config config) {
 		//		apply all config changes from base scenario class
 		super.prepareConfig(config);
 
-//		TODO: is it enough to change teleportedModeSpeed of bike?
+//		bike is teleported, but routed on the network in berlin 6.4
+//		this means that there are no teleported mode params for bike. bike speed is set via vehicleType or freespeed
+		if (config.routing().getTeleportedModeParams().get(TransportMode.bike) != null) {
+			log.fatal("Found teleported mode params for {}." +
+				" In matsim-berlin v6.4 {} is routed on the network, so there should not be teleported mode params for this mode! Aborting!", TransportMode.bike, TransportMode.bike);
+			throw new IllegalStateException("");
+		}
 
 		return config;
 	}
@@ -32,7 +44,11 @@ public class OpenBerlinBikeSpeedScenario extends OpenBerlinScenario {
 	public void prepareScenario(Scenario scenario) {
 		//		apply all scenario changes from base scenario class
 		super.prepareScenario(scenario);
-//		TODO: do we need to change the speed of veh type bike? Or is this value not used when teleported?!
+//		original bike speed of berlin 6.4 is 2.98 m/s = 10.728 km/h
+		if (maxBikeSpeedKmH != 10.728) {
+			scenario.getVehicles().getVehicleTypes().get(Id.create(TransportMode.bike, VehicleType.class)).setMaximumVelocity(maxBikeSpeedKmH / 3.6);
+			log.info("Maximum velocity for {} was set to {} km/h. Default is 10.728 km/h. Make sure this is what you want.", TransportMode.bike, maxBikeSpeedKmH);
+		}
 	}
 
 	@Override
