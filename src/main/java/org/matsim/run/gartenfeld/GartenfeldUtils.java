1aa2bd2589aee6cb75665591a654e2e874112f10
sime94 <meinhardt@vsp.tu-berlin.de>
2026-02-04 17:45:25 +0100

    some bug fixes

--- src/main/java/org/matsim/run/gartenfeld/GartenfeldUtils.java
+++ src/main/java/org/matsim/run/gartenfeld/GartenfeldUtils.java
@@ -13,6 +13,7 @@ import org.matsim.vehicles.EngineInformation;
 import org.matsim.vehicles.VehicleType;
 import org.matsim.vehicles.VehicleUtils;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -74,19 +75,40 @@ public final class GartenfeldUtils {
 			.values().forEach(type -> VehicleUtils.setHbefaVehicleCategory(type.getEngineInformation(), HbefaVehicleCategory.NON_HBEFA_VEHICLE.toString()));
 	}
 
+	/**
+	 * Disable wrap-around scoring of first and last act of the day by setting them to different subtypes "_morning" and "_evening".
+	 */
 	static void changeWrapAroundActsIntoMorningAndEveningActs(Scenario scenario) {
 		Set<String> firstActTypes = new HashSet<>();
 		Set<String> lastActTypes = new HashSet<>();
 
 		for (Person p : scenario.getPopulation().getPersons().values()) {
+//			ignore freight / commercial traffic agents and stay home agents
+			if (!p.getAttributes().getAttribute("subpopulation").equals("person") ||
+			p.getSelectedPlan().getPlanElements().size() == 1) {
+				continue;
+			}
+
 			for (Plan plan : p.getPlans()) {
 				Activity first = (Activity) plan.getPlanElements().getFirst();
 				Activity last = (Activity) plan.getPlanElements().getLast();
 
 				String[] splitFirst = first.getType().split("_");
-
-				String typeFirst = splitFirst[0];
+				String typeFirst = String.join("_", Arrays.copyOfRange(splitFirst, 0, splitFirst.length - 1));
+				int orginalTimeBinFirst = Integer.parseInt(splitFirst[splitFirst.length - 1]);
 				firstActTypes.add(typeFirst);
+
+				String[] splitLast = last.getType().split("_");
+				String typeLast = String.join("_", Arrays.copyOfRange(splitLast, 0, splitLast.length - 1));
+				int orginalTimeBinLast = Integer.parseInt(splitLast[splitLast.length - 1]);
+				lastActTypes.add(typeLast);
+
+				if (!typeFirst.equals(typeLast)) {
+//					if first and last act do not have the same type, we will not change anything.
+//					this is the pragmatic version. There are last acts with without startTime, endTime or maxDuration.
+//					this needs to be repaired upstream (in the makefile process). -sm0226
+					continue;
+				}
 
 				Double durationFirst = null;
 				if (first.getEndTime().isDefined()) {
@@ -103,35 +125,25 @@ public final class GartenfeldUtils {
 					throw new IllegalStateException("");
 				}
 
-				int durationBin = getDurationBin(durationFirst);
+				int durationBinFirst = getDurationBin(durationFirst);
 
-				first.setType(String.format("%s_%d", Activities.createMorningActivityType(typeFirst), durationBin));
+				first.setType(String.format("%s_%d", Activities.createMorningActivityType(typeFirst), durationBinFirst));
 
-				String[] splitLast = last.getType().split("_");
-				String typeLast = splitLast[0];
-				lastActTypes.add(typeLast);
-
-//				duration bins of first and last act have to match
-				if (!splitFirst[1].equals(splitLast[1])) {
-					log.fatal("typical duration of first and last activity of person {} are not the same. This should not happen, aborting!", p.getId());
+	//			act types of first and last act the same
+				if (orginalTimeBinFirst != orginalTimeBinLast) {
+					log.fatal("typical duration of first and last activity of person {} with the same act type {} are not the same. This should not happen, aborting!", p.getId(), typeLast);
 					throw new IllegalStateException("");
 				}
-
-				if (!typeFirst.equals(typeLast)) {
-					log.warn("Types of first activity ({}) and last activity ({}) do not match.", typeFirst, typeLast);
-				}
-
-				double durationLast = Double.parseDouble(splitFirst[2]) - durationFirst;
+				double durationLast = orginalTimeBinLast - durationFirst;
 
 				last.setType(String.format("%s_%d", Activities.createEveningActivityType(typeLast), getDurationBin(durationLast)));
 				last.setMaximumDuration(durationLast);
 				last.setEndTimeUndefined();
 				last.setStartTimeUndefined();
-
-				log.info("Activity types of first activity in plans: {}", firstActTypes);
-				log.info("Activity types of last activity in plans: {}", lastActTypes);
 			}
 		}
+		log.info("Activity types of first activity in plans: {}", firstActTypes);
+		log.info("Activity types of last activity in plans: {}", lastActTypes);
 	}
 
 	private static int getDurationBin(Double duration) {
